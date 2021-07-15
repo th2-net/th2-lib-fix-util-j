fa3038fee4daae782fb2b0f5ac4802423df9564b
Levan Gharibashvili <levan.gharibashvili@tbl019.exp.exactpro.com>
2021-07-15 12:47:15 +0400

    Created utility method for generating ClOrdID. Updated project information.

--- src/main/java/com/exactpro/th2/lib/fix/Util.kt
+++ src/main/java/com/exactpro/th2/lib/fix/Util.kt
@@ -5,25 +5,23 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
+package com.exactpro.th2.lib.fix
 
-package com.exactpro.th2.lib.template;
+import java.time.ZonedDateTime
+import java.time.temporal.Temporal
 
-import java.util.Random;
+class Util {
 
-public class Util {
-    public static String generateHexString() {
-        return Long.toHexString(Double.doubleToLongBits(Math.random()));
-    }
-
-    public static int generateInteger(int bound) {
-        return new Random().nextInt(bound);
+    private var nextID = 1;
+    fun generateClOrdID(): String {
+        return (System.currentTimeMillis() + nextID++).toString();
     }
 }
